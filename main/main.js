f091615f7987761baa41cdf634c8b89b7acd05cb
xiallyspider <33478703+xiallyspider@users.noreply.github.com>
2018-04-18 11:08:22 +0800

    logic

--- main/main.js
+++ main/main.js
@@ -1,6 +1,60 @@
 // Write your cade below:
-function main() {};
+var readlineSync = require('readline-sync')
 
-module.exports = {
-    main
+function createRandom(n) {
+    var rdm = ''
+    for(let x = 0; x < n; x++){
+        rdm += Math.floor(Math.random()*10)
+    }
+    return rdm
+}
+function verifyInput(random, input) {
+    let result = []
+    let randomArr = random.split('')
+    let inputArr = input.split('')
+    let aCount = 0
+    let bCount = 0
+    for (let x = 0; x < randomArr.length; x++) {
+        if(randomArr[x] == inputArr[x]){
+            aCount ++
+        }
+    }
+    let tempArr = randomArr
+    inputArr.forEach(element => {
+       for (let y = 0; y < tempArr.length; y++) {
+           if(element === tempArr[y]){
+                tempArr.splice(y, 1)
+                bCount ++
+                break
+           }
+       }
+   });
+   bCount = bCount - aCount
+   result = [aCount, bCount]
+   return result
+}
+module.exports = function main(rdm, ipt) {
+    console.log("开始宾果游戏！")
+    let result = ''
+    let resultArr = []
+    let random = rdm
+    // random = createRandom(4)
+    let input = ipt
+    // input = readlineSync.question('');
+    // for(let times = 0; times < 9; times++){
+    //     if (input === '' || input.split('').length !== 4) {
+    //         console.log("请输入4个数字（1-9之间）！")
+    //         input = readlineSync.question('');
+    //     } else {
+    //         resultArr = verifyInput(random, input)
+    //         result = resultArr[0] + 'A' + resultArr[1] + 'B'
+    //         console.log(result)
+    //         return result
+    //     }
+    //     times++
+    // }
+    resultArr = verifyInput(random, input)
+    result = resultArr[0] + 'A' + resultArr[1] + 'B'
+    console.log(result)
+    return result
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,32 +1,25 @@
 const main = require('../main/main');
 
 describe('main()', () => {
-  // function createRandom(n) {
-  //     var rdm = ''
-  //     for(let x = 0; x < n; x++){
-  //       rdm += Math.floor(Math.random()*10)
-  //     }
-  // }
-  
   it("1A2B",function() {
-    let rdm = 1211
-    let input = 1122
+    let rdm = '1211'
+    let input = '1122'
     let result = main(rdm, input)
     let expected = '1A2B'
     expect(result).toEqual(expected)
   })
 
   it("2A0B",function() {
-    let rdm = 1211
-    let input = 1315
+    let rdm = '1211'
+    let input = '1315'
     let result = main(rdm, input)
     let expected = '2A0B'
     expect(result).toEqual(expected)
   })
 
   it("0A4B",function() {
-    let rdm = 5463
-    let input = 3645
+    let rdm = '5463'
+    let input = '3645'
     let result = main(rdm, input)
     let expected = '0A4B'
     expect(result).toEqual(expected)
